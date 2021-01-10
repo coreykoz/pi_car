b60bfc9bea93679e75f7f60a721c3a94b7314f4a
coreykoz <ckozlovski2017@gmail.com>
2021-01-09 20:17:09 -0500

    url patterns change

--- picar_server/picar/views.py
+++ picar_server/picar/views.py
@@ -29,16 +29,15 @@ class StreamingOutput(object):
                     self.buffer.seek(0)
                 return self.buffer.write(buf)
 
-with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-        output = StreamingOutput()
-        #Uncomment the next line to change your Pi's Camera rotation (in degrees)
-        #camera.rotation = 90
-        camera.start_recording(output, format='mjpeg')
-
 def index(request):
     return render(request, "picar/index.html")
 
 def videoStream(request):
+    with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+        output = StreamingOutput()
+        #Uncomment the next line to change your Pi's Camera rotation (in degrees)
+        #camera.rotation = 90
+        camera.start_recording(output, format='mjpeg')
     response = HttpResponse()
     response['Age'] = 0
     response['Cache-Control'] = 'no-cache, private'
