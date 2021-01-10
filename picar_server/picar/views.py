023013f2e9c90bc8af831d2c6a3ca0f8782411cf
coreykoz <ckozlovski2017@gmail.com>
2021-01-09 20:30:13 -0500

    fixing stream

--- picar_server/picar/views.py
+++ picar_server/picar/views.py
@@ -13,50 +13,49 @@ from threading import Condition
 import codecs
 
 class StreamingOutput(object):
-            def __init__(self):
-                self.frame = None
-                self.buffer = io.BytesIO()
-                self.condition = Condition()
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
 
-            def write(self, buf):
-                if buf.startswith(b'\xff\xd8'):
-                    # New frame, copy the existing buffer's content and notify all
-                    # clients it's available
-                    self.buffer.truncate()
-                    with self.condition:
-                        self.frame = self.buffer.getvalue()
-                        self.condition.notify_all()
-                    self.buffer.seek(0)
-                return self.buffer.write(buf)
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
 
-def index(request):
-    return render(request, "picar/index.html")
-
-def videoStream(request):
-    with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
         output = StreamingOutput()
         #Uncomment the next line to change your Pi's Camera rotation (in degrees)
         #camera.rotation = 90
         camera.start_recording(output, format='mjpeg')
         print("camera established")
+
+def index(request):
+    return render(request, "picar/index.html")
+
+def videoStream(request):
     response = HttpResponse()
     response['Age'] = 0
     response['Cache-Control'] = 'no-cache, private'
     response['Pragma'] = 'no-cache'
     response['Content-Type'] = 'multipart/x-mixed-replace; boundary=FRAME'
     print("initial response headers printed")
-    try:
-        while True:
-            with output.condition:
-                output.condition.wait()
-                frame = output.frame
-            response.write(b'--FRAME\r\n')
-            response['Content-Type'] = 'text/html'
-            response['Content-Length'] = len(frame)
-            response.write(frame)
-            response.write(b'\r\n')
-            print(response)
-            return response
-    finally:
-        camera.stop_recording()
+    while True:
+        with output.condition:
+            output.condition.wait()
+            frame = output.frame
+            print("updated frame")
+        response.write(b'--FRAME\r\n')
+        response['Content-Type'] = 'text/html'
+        response['Content-Length'] = len(frame)
+        response.write(frame)
+        response.write(b'\r\n')
+        print(response)
+        return response
     
